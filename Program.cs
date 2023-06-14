4dfd4d02446bb4d22f8244619a7d72d6f2fa5fe2
Clemens Schneider <clemens.schneider.ch@gmail.com>
2023-06-14 15:54:58 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,40 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines(args[0]);
+
+        using (StreamWriter writer = new(args[1]))
+        {
+            var columns = lines[0].Split(',');
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                columns[i] = $"[{columns[i][1..^1]}]";
+            }
+
+            lines[0] = string.Join(',', columns);
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                if ((i - 1) % 1000 == 0)
+                {
+                    writer.WriteLine($"INSERT INTO LNK ({lines[0]})");
+                    writer.WriteLine("VALUES");
+                }
+
+                if (i % 1000 == 0 || i == lines.Length - 1)
+                {
+                    writer.WriteLine($"({lines[i]});");
+                    writer.WriteLine("GO");
+                }
+                else
+                {
+                    writer.WriteLine($"({lines[i]}),");
+                }
+            }
+        }
+
+        Console.WriteLine("File updated successfully.");
+    }
+}
